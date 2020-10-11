e3e3e524b1624ef4a0e77df35c743143181b261a
Tron930123 <jiahuiyang0@gmail.com>
2020-10-11 15:22:30 +1100

    JL format converter

---
+++ jl_format_convert.py
@@ -0,0 +1,23 @@
+"""Format converter for label_jl
+
+"""
+import json
+import os
+dirs = os.listdir(".")
+#print(dir)
+for i in dirs:
+    if os.path.splitext(i)[1] == ".json":
+        with open(i,'r',encoding='utf8') as f:
+            #line = f.readline()
+            data = json.load(f)
+            print(i)
+            new_file = open("label_new/"+str(os.path.splitext(i)[0])+'.txt', 'w+')
+            for i in range(0,len(data['outputs']['object'])):
+                prefix = data['outputs']['object'][i]
+                clss = prefix['name']
+                box_lt = [prefix['bndbox']['xmin'], prefix['bndbox']['ymin']]
+                box_rb = [prefix['bndbox']['xmax'], prefix['bndbox']['ymax']]
+                result = str(clss)+" 0.00 0 0.0 "+str(int(box_lt[0]))+" "+str(int(box_lt[1]))+" "+str(int(box_rb[0]))+" "+str(int(box_rb[1]))+" 0.0 0.0 0.0 0.0 0.0 0.0 0.0\n"
+                print(result)
+                new_file.write(result)
+                new_file.close()
